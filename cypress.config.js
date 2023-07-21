61faa2c0538f02538bb49a26092cea6193f782b3
raihanannafi <raihan.annafi@shipper.id>
2023-07-21 15:44:26 +0700

    add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Raihan Annafi',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportFilename: 'report'
+  },
   e2e: {
     baseUrl: 'https://reqres.in',
     screenshotOnRunFailure: false,
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
